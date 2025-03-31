f833c3bd93d6d6b42b915f68cb6c2c06a94bbce3
achig4tsu <starflo.83@gmail.com>
2025-03-31 02:22:34 +0200

    Fix front

--- app/frontend/nuxt.config.ts
+++ app/frontend/nuxt.config.ts
@@ -1,43 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-
-  modules: [
-    '@nuxtjs/tailwindcss',
-    '@nuxt/fonts',
-    '@nuxt/icon',
-    '@nuxt/scripts',
-    '@nuxt/test-utils',
-    '@nuxt/eslint',
-    'shadcn-nuxt'
-  ],
-  
-  tailwindcss: {
-    exposeConfig: true,
-    viewer: true,
-    // and more...
-  },
-
-  shadcn: {
-      /**
-       * Prefix for all the imported component
-       */
-      prefix: '',
-      /**
-       * Directory that the component lives in.
-       * @default "./components/ui"
-       */
-      componentDir: './components/ui'
-    },
-
+  modules: ['@nuxtjs/tailwindcss'],
   runtimeConfig: {
     public: {
-      apiUrl: process.env.API_URL || 'http://localhost:4000',
+      apiUrl: process.env.API_URL || 'http://localhost:4040',
       cloudflareSiteKey: process.env.CLOUDFLARE_SITE_KEY
     }
   },
-
   app: {
     head: {
       script: [
@@ -49,11 +19,10 @@ export default defineNuxtConfig({
       ]
     }
   },
-
   nitro: {
     routeRules: {
       '/challenge': { appMiddleware: { auth: false } },
       '/**': { appMiddleware: ['auth'] }
     }
   }
-})
+});
