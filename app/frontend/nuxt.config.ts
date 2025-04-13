3308d2a4e2dc4295856f4d0517a45c38351fd0dd
achig4tsu_ <meiosei.gaming@gmail.com>
2025-04-13 19:01:15 +0200

    ReadMe

--- app/frontend/nuxt.config.ts
+++ app/frontend/nuxt.config.ts
@@ -15,7 +15,7 @@ export default defineNuxtConfig({
   css: ['~/assets/css/tailwind.css'],
   postcss: {
     plugins: {
-      '@tailwindcss/postcss': {},
+      tailwindcss: {},
       autoprefixer: {},
     },
   },
